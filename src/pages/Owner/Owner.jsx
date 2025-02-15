ddd024bf2e391608e332183235454fba81da08c8
hyeryunYou <hyeryun1108@naver.com>
2025-02-16 03:45:14 +0900

    owner 페이지 진행상황 커밋

--- src/pages/Owner/Owner.jsx
+++ src/pages/Owner/Owner.jsx
@@ -1,7 +1,8 @@
 import CalendarComponent from "../../Components/Calendar";
+import ReactCalendar from "../../Components/Calendar";
 import styled from "styled-components";
 import ReservationCard from "../../Components/ReservationCard";
-import { userReservation } from "../../assets/userReservation";
+//import { userReservation } from "../../assets/userReservation";
 import moment from "moment";
 import { useState, useEffect } from "react";
 import { checkMobile } from "../../utils/checkMobile";
@@ -39,48 +40,35 @@ const ReservationLabel = styled.p`
     margin-bottom: 3%;
 `;
 
-const Owner = () => {
+const Owner = (reservations) => {
     const [selectedDate, setSelectedDate] = useState(
         moment().format("YYYY-MM-DD")
     );
+    const isMobile = checkMobile();
 
     // 날짜 선택 시 처리 함수
-    const handleDateSelect = (date) => {
+    const handleDateSelect = (date, reservationData) => {
         const formattedDate = moment(date).format("YYYY-MM-DD"); // 날짜 포맷
         setSelectedDate(formattedDate); // 상태에 저장
+        reservations(reservationData || []);
     };
 
     useEffect(() => {
         console.log(selectedDate);
-    }, [selectedDate]);
-    const isMobile = checkMobile();
+        console.log(reservations);
+    }, [selectedDate, reservations]);
 
     // API로 대여자 예약 내역 조회 구현
     return (
         <Container ismobile={isMobile}>
             <CalendarWrapper>
                 <CalendarComponent onDateSelect={handleDateSelect} />
+                <ReactCalendar/>
             </CalendarWrapper>
+
             <ReservationWrapper>
                 <ReservationLabel>예약 내역</ReservationLabel>
-                {userReservation.result.resultList.map((el) => (
-                    <ReservationCard
-                        img={
-                            el.reservationResult.practiceRoomDetail
-                                .practiceRoomDetailImage
-                        }
-                        roomName={
-                            el.reservationResult.practiceRoom.PracticeRoomName
-                        }
-                        detailName={
-                            el.reservationResult.practiceRoomDetail
-                                .practiceRoomDetailName
-                        }
-                        date={el.reservationResult.date}
-                        startTime={el.reservationResult.startTime}
-                        endTime={el.reservationResult.endTime}
-                    />
-                ))}
+                
             </ReservationWrapper>
         </Container>
     );
